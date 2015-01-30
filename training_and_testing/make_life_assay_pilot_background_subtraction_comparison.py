8218013f93db407c0b29900c88b2d5bac47beea3
Erik Hvatum <ice.rikh@gmail.com>
2015-01-29 18:22:06 -0600

    Finish implementing make_life_assay_pilot_background_subtraction_comparison.py.

--- training_and_testing/make_life_assay_pilot_background_subtraction_comparison.py
+++ training_and_testing/make_life_assay_pilot_background_subtraction_comparison.py
@@ -34,8 +34,18 @@ import sys
 from wautofocuser.training_and_testing.image_from_multi_image_statistics import generate_running_percentile_differences
 from PyQt5 import Qt
 
+def normalize_intensity(im):
+    imn = im.astype(numpy.float32)
+    imn *= 255/im.max()
+    imn = imn.astype(numpy.uint8)
+    return numpy.dstack((imn, imn, imn, imn))
+
 def make_it(write_output=True, output_frame_count_limit=None):
     """If write_output is False, an array of the images rendered is returned."""
+
+    background_model_frame_count = 4
+    background_model_percentile = 90
+
     output_dpath = Path('/home/ehvatum/oot')
     experiment_dpath = Path('/mnt/scopearray/life_assay_pilot')
     do_well_idxs = [0,1,2,3,4,6]
@@ -49,12 +59,13 @@ def make_it(write_output=True, output_frame_count_limit=None):
     frame_outer_gap = Qt.QSizeF(8,8)
     background_c = (0,0,0,255)
     image_background_c = (127,127,127,255)
-    title_text_c = (0,200,0,255)
-    data_accruing_text_c = (127,0,0,255)
+    title_text_c = (200,200,200,255)
+    name_text_c = (0,200,0,255)
+    data_accruing_text_c = (255,0,0,255)
     text_height = 80
     text_gap = 8
-    canvas_size = Qt.QSizeF((crop.width() + image_frame.width()*2 + frame_outer_gap.width()*2) * 3,
-                            (text_height + text_gap*2)*2 + crop.height() + image_frame.height()*2 + frame_outer_gap.height()*2)
+    canvas_size = Qt.QSizeF((crop.width() + image_frame.width()*2)*3 + frame_outer_gap.width()*4,
+                            text_height * 2 + text_gap * 3 + crop.height() + image_frame.height()*2 + frame_outer_gap.height()*2)
 
     canvas = pyagg.Agg2D(numpy.empty((canvas_size.height(), canvas_size.width(), 4), dtype=numpy.uint8))
     canvas.flip_text = True
@@ -67,15 +78,84 @@ def make_it(write_output=True, output_frame_count_limit=None):
     output_frame_idx = 0
     for well_dpath, well_idx in zip(well_dpaths, do_well_idxs):
         bf_fpaths = sorted(well_dpath.glob('life_assay_pilot__*_bf.tiff'))
-        for bf_fpath in bf_fpaths:
+        bg_subtracter = generate_running_percentile_differences(bf_fpaths, background_model_percentile, background_model_frame_count)
+        for bf_fpath, bg_delta in zip(bf_fpaths, bg_subtracter):
             run_idx = int(bf_fpath.stem.split('_')[-2])
             canvas.buffer[...,:] = background_c
             canvas.set_fill_color(*title_text_c)
             ts_str = datetime.datetime.fromtimestamp(bf_fpath.stat().st_ctime).isoformat()[:19]
-            canvas.text(int(canvas_size.width() / 2), text_gap + text_height/2, 'well {} run {:04} ({})'.format(well_idx, run_idx, ts_str))
+            title_text_pos = numpy.array((int(canvas_size.width() / 2), text_gap + text_height/2), dtype=numpy.float64)
+            canvas.text(title_text_pos[0], title_text_pos[1], 'well {} run {:04} ({})'.format(well_idx, run_idx, ts_str))
+
+            bf = skio.imread(str(bf_fpath))
+            canvas.set_fill_color(*image_background_c)
+            image_frame_rect = Qt.QRectF(Qt.QPointF(0,0), Qt.QSizeF(crop.width()+2*image_frame.width(), crop.height()+2*image_frame.height()))
+            image_frame_rect.translate(0, text_gap*3 + text_height*2)
+            image_frame_rect.translate(frame_outer_gap.width(), frame_outer_gap.height())
+            image_rect = Qt.QRectF(crop)
+            image_rect.translate(image_frame_rect.left() + image_frame.width(), image_frame_rect.top() + image_frame.height())
+            image_rect.translate(-crop.left(), -crop.top())
+            canvas.rectangle(image_frame_rect.left(), image_frame_rect.top(), image_frame_rect.right(), image_frame_rect.bottom())
+            canvas.buffer[image_rect.top():image_rect.bottom(),
+                          image_rect.left():image_rect.right(), :] = normalize_intensity(bf[crop.top():crop.bottom(), crop.left():crop.right()])
+            title_text_pos[0] = int((image_frame_rect.right() + image_frame_rect.left())/2)
+            title_text_pos[1] += text_height + text_gap
+            canvas.set_fill_color(*name_text_c)
+            canvas.text(title_text_pos[0], title_text_pos[1], 'BF')
+
+            canvas.set_fill_color(*image_background_c)
+            image_frame_rect.translate(image_frame.width() * 2 + crop.width() + frame_outer_gap.width(), 0)
+            canvas.rectangle(image_frame_rect.left(), image_frame_rect.top(), image_frame_rect.right(), image_frame_rect.bottom())
+            title_text_pos[0] = int((image_frame_rect.right() + image_frame_rect.left())/2)
+            canvas.set_fill_color(*name_text_c)
+            canvas.text(title_text_pos[0], title_text_pos[1], 'abs(BF - background_model)')
+            image_rect = Qt.QRectF(crop)
+            image_rect.translate(image_frame_rect.left() + image_frame.width(), image_frame_rect.top() + image_frame.height())
+            image_rect.translate(-crop.left(), -crop.top())
+            if bg_delta is None:
+                data_accruing_text_pos = title_text_pos.copy()
+                data_accruing_text_pos[1] = int((image_rect.top() + image_rect.bottom())/2)
+                canvas.set_fill_color(*data_accruing_text_c)
+                canvas.text(data_accruing_text_pos[0], data_accruing_text_pos[1], 'building model... ({}/{})'.format(run_idx+1, background_model_frame_count))
+            else:
+                canvas.buffer[image_rect.top():image_rect.bottom(),
+                              image_rect.left():image_rect.right(), :] = normalize_intensity(bg_delta[crop.top():crop.bottom(), crop.left():crop.right()])
+
+            canvas.set_fill_color(*image_background_c)
+            image_frame_rect.translate(image_frame.width() * 2 + crop.width() + frame_outer_gap.width(), 0)
+            canvas.rectangle(image_frame_rect.left(), image_frame_rect.top(), image_frame_rect.right(), image_frame_rect.bottom())
+            title_text_pos[0] = int((image_frame_rect.right() + image_frame_rect.left())/2)
+            canvas.set_fill_color(*name_text_c)
+            fluo_fpath = well_dpath / 'life_assay_pilot__{:04}_{:04}_fluo.tiff'.format(well_idx, run_idx)
+            if fluo_fpath.exists():
+                canvas.text(title_text_pos[0], title_text_pos[1], 'fluo (cyan)')
+                fluo = skio.imread(str(fluo_fpath))
+            else:
+                fluo_fpath = well_dpath / 'life_assay_pilot__{:04}_{:04}_fluo_greenyellow.tiff'.format(well_idx, run_idx)
+                if fluo_fpath.exists():
+                    canvas.text(title_text_pos[0], title_text_pos[1], 'fluo (green yellow)')
+                    fluo = skio.imread(str(fluo_fpath))
+                else:
+                    # This case can happen if acquisition for this well is in progress such that a BF
+                    # image for this run has been acquired while the fluo image has not yet been acquired.
+                    canvas.text(title_text_pos[0], title_text_pos[1], 'fluo (?)')
+                    data_accruing_text_pos = title_text_pos.copy()
+                    data_accruing_text_pos[1] = int((image_rect.top() + image_rect.bottom())/2)
+                    canvas.set_fill_color(*data_accruing_text_c)
+                    canvas.text(data_accruing_text_pos[0], data_accruing_text_pos[1], 'file not found')
+                    fluo = None
+            if fluo is not None:
+                image_rect = Qt.QRectF(crop)
+                image_rect.translate(image_frame_rect.left() + image_frame.width(), image_frame_rect.top() + image_frame.height())
+                image_rect.translate(-crop.left(), -crop.top())
+                canvas.buffer[image_rect.top():image_rect.bottom(),
+                              image_rect.left():image_rect.right(), :] = normalize_intensity(fluo[crop.top():crop.bottom(), crop.left():crop.right()])
+
             out_image = ndimage.zoom(canvas.buffer[...,:3], (0.5, 0.5, 1), order=1)
+#           out_image = canvas.buffer[...,:3].copy()
             if write_output:
-                pass
+                out_fpath = output_dpath / '{:06}.bmp'.format(output_frame_idx)
+                skio.imsave(str(out_fpath), out_image)
             else:
                 ret.append(out_image)
             output_frame_idx += 1
